1db7259836ee0b834d993551687a68a6a85e0a09
ScottUK <scottukgaming@gmail.com>
2018-11-15 18:09:46 +0000

    Update server.lua

--- server.lua
+++ server.lua
@@ -9,8 +9,14 @@ AddEventHandler("EVL:RequestConfigs", function()
 
 	for a = 1, #EVL["Base"]["LoadPatterns"] do
 		local pattern = LoadResourceFile(GetCurrentResourceName(), "./patterns/" .. EVL["Base"]["LoadPatterns"][a] .. ".json")
-		patterns[EVL["Base"]["LoadPatterns"][a]] = pattern
+		patterns[EVL["Base"]["LoadPatterns"][a]] = json.decode(pattern)
+		print("[EVL]: Loaded Pattern " .. EVL["Base"]["LoadPatterns"][a])
 	end
 
 	TriggerClientEvent("EVL:LoadConfigs", src, EVL, patterns)
+end)
+
+RegisterServerEvent('ELV:SyncSiren')
+AddEventHandler('ELV:SyncSiren', function(vehicle, siren)
+    TriggerClientEvent('ELV:SyncSiren', -1, vehicle, siren)
 end)
